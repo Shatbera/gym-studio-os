83121f7fdd84559d256dafc305f6ec7c4321ca7b
Shatbera <81585007+Shatbera@users.noreply.github.com>
2025-09-04 23:41:13 +0400

    #init project, ignore lib folder and .vs folder

---
+++ GymStudioOS/GymStudioOS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GymStudioOS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
